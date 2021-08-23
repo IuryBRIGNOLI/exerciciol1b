6425863360dd88373ebaf134924b4e5862e20547
IuryBRIGNOLI <iury.195018@alunosatc.edu.br>
2021-08-23 15:50:00 -0300

    Create ex1.py

---
+++ ex1.py
@@ -0,0 +1,21 @@
+# print ("Olá mundo") 
+# nome = input ("Digite seu nome ")
+# print ("Seja bem vindo " + nome )
+#n1 = float(input("Digite um número "))
+#n2 = float(input("Digite outro número "))
+#s = n1 + n2 
+#print(s)
+#dia = input("dia que nasceu")
+#mes = input("mês que nasceu")
+#ano = input("ano que nasceu ")
+#print(dia+"/"+mes+"/"+ano)
+n1 = float(input("Digite um número"))
+s0 = n1 
+s = n1*2
+s2 = n1*3
+s3 =n1//1/2
+print (s0)
+print(s)
+print(s2)
+print(s3)
+
